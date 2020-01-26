55e8e8bfac88260b18b446bf342460a1bbdbf5c8
Dan Certa <dcerta@oneeightycapital.com>
2020-01-26 13:39:15 -0600

    finish part 1

--- FSharpConApp/Program.fs
+++ FSharpConApp/Program.fs
@@ -1,8 +1,30 @@
 ﻿// Learn more about F# at http://fsharp.org
-
 open System
+open ClassLibrary.World
+open FSharpConsoleApp.Display
+
+let generateWorld randomizer =
+    new World(8,8, randomizer)
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    printfn "F# Console Application Tutorial"
+
+    let randomizer = new Random()
+
+    let mutable simulating: bool = true
+    let mutable world = generateWorld(randomizer)
+
+    while simulating do
+        displayWorld world
+
+        let key = getUserInput()
+
+        Console.Clear()
+
+        match key.Key with
+        | ConsoleKey.X -> simulating <- false
+        | ConsoleKey.R -> world <- generateWorld(randomizer)
+        | _ -> printfn "Invalid input '%c'" key.KeyChar
+
     0 // return an integer exit code
